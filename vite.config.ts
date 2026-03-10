028ab4975f3d990c2e4c8cd5461685dcc70d71fc
jhuliangr <jhpiano9731@gmail.com>
2026-03-10 16:04:37 +0100

    Fixing routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
@@ -5,6 +6,7 @@ import { resolve } from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: process.env.BASE_URL,
   test: {
     globals: true,
     environment: 'jsdom',
