7f61af85087edb3ffe213661cda1824a95ba0b74
jhuliangr <jhpiano9731@gmail.com>
2026-03-17 10:00:57 +0100

    Merging PR

--- src/games/Wordle/Guesses/Cell/Cell.tsx
+++ src/games/Wordle/Guesses/Cell/Cell.tsx
@@ -2,5 +2,13 @@ import type { Cell } from '#shared/types';
 import styles from './Cell.module.css';
 
 export const CellComponent: React.FC<Cell> = ({ letter, state }) => {
-  return <div className={`${styles['box']} ${styles[state]}`}>{letter}</div>;
+  return (
+    <div
+      className={`${styles['box']} ${styles[state]}`}
+      data-testid="wordle-cell"
+      data-state={state}
+    >
+      {letter}
+    </div>
+  );
 };
